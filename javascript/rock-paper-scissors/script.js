e5e252cd1192bde8ccf42740c4e45581e386f7ab
ashalexis <59964978+ashalexis@users.noreply.github.com>
2020-02-27 09:10:04 +1100

    Create script.js

---
+++ javascript/rock-paper-scissors/script.js
@@ -0,0 +1,51 @@
+//Rock, Paper, Scissors.
+
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  //checking whether the user gives a valid choice
+  if (userInput==='rock' || userInput==='scissors' || userInput==='paper' || userInput==='bomb'){return userInput}
+  else {return 'invalid choice'}
+};
+
+const getComputerChoice = () => {
+  const options = ['rock','paper','scissors'];
+  const choice = Math.floor(Math.random()*3);
+  return options[choice]
+};
+
+const determineWinner = (userChoice,computerChoice) => {
+  if (userChoice==='bomb'){
+    return 'why are you using a cheat code'
+  }
+  else if (userChoice===computerChoice){
+    return 'your game was a tie'
+  }
+  else if(userChoice==='rock'){
+    if (computerChoice==='paper'){
+      return 'the computer won!'
+    }
+    else {return 'you won!'}
+  }
+  else if(userChoice==='paper'){
+    if (computerChoice==='scissors'){
+      return 'the computer won!'}
+    else {return 'you won!'}
+    }
+  else if(userChoice==='scissors'){
+    if (computerChoice==='rock'){
+      return 'the computer won!'
+    }
+    else {return 'you won!'}
+  }
+  }
+
+
+// playGame allows you to put in userInput
+const playGame = (userInput) => {
+  const userChoice = getUserChoice(userInput);
+  const computerChoice = getComputerChoice();
+  console.log(determineWinner(userChoice,computerChoice))
+}
+
+// game example
+playGame('rock')
